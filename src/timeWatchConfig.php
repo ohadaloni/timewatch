b0c71772a2b4b507a2ce07433d78ec8423f1e027
ohad <ohad@theora.com>
2020-07-27 10:41:36 +0300

    SnapShot

--- src/timeWatchConfig.php
+++ src/timeWatchConfig.php
@@ -7,5 +7,7 @@ date_default_timezone_set("UTC");
 define('M_DIR', "/var/www/vhosts/M.theora.com");
 define('TAS_DIR', "/var/www/vhosts/tas.theora.com");
 require_once(TAS_DIR."/conf/dbCredentials.php");
+require_once(TAS_DIR."/conf/reCaptchaKeys.php");
+require_once(TAS_DIR."/libs/recaptchalib.php");
 define('M_DBNAME', 'timewatch');
 /*------------------------------------------------------------*/
