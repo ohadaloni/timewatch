61eef63db82a1eff3b6f1059fd66926065214bf1
ohad <ohad@theora.com>
2020-09-06 12:52:15 +0300

    gitDiffs

--- src/index.php
+++ src/index.php
@@ -6,6 +6,17 @@ require_once(M_DIR."/mfiles.php");
 require_once("timeWatchFiles.php");
 require_once("TimeWatch.class.php");
 /*------------------------------------------------------------*/
+$ua = @$_SERVER['HTTP_USER_AGENT'];
+if (
+	! $ua
+	|| stristr($ua, "bot")
+	|| stristr($ua, "crawl")
+	|| stristr($ua, "spider")
+	) {
+	http_response_code(204);
+	exit;
+}
+/*------------------------------------------------------------*/
 global $Mview;
 global $Mmodel;
 $Mview = new Mview;
