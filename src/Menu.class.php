8d2bc0deda82f21abec3a037a553fa8ee3bc08d1
ohad <ohad@theora.com>
2020-12-19 11:42:34 +0200

    gitDiffs

--- src/Menu.class.php
+++ src/Menu.class.php
@@ -2,14 +2,83 @@
 /*------------------------------------------------------------*/
 class Menu extends Mcontroller {
 	/*------------------------------------------------------------*/
-	public function index() {
+	public function index($project = null) {
 			$this->Mview->showTpl("menuDriver.tpl", array(
-				'menu' => $this->dd(),
+				'menu' => $this->dd($project),
 			));
 	}
 	/*------------------------------------------------------------*/
 	/*------------------------------------------------------------*/
-	private function dd() {
+	private function dd($thisProject) {
+		$loginName = TimeWatchLogin::loginName();
+		if ( $loginName ) {
+			$sql = "select distinct project from timewatch where  user = '$loginName'";
+			$projects = $this->Mmodel->getStrings($sql);
+			if ( $thisProject )
+				$projectMenuTitle = "project: $thisProject";
+			else
+				$projectMenuTitle = "project";
+			$projectMenu = array();
+			foreach ( $projects as $project ) {
+				if ( $project == $thisProject )
+					continue;
+				$projectMenu[] = array(
+					'name' => $project,
+					'title' => $project,
+					'url' => "/timeWatch/setProject?project=$project",
+				);
+			}
+			if ( $projectMenu )
+				$projectMenu[] = array(
+					'divider' => '-----------------------',
+			);
+			$projectMenu[] = array(
+				'name' => 'newProject',
+				'title' => 'newProject',
+				'url' => "/timeWatch/newProject",
+			);
+			$projectMenu[] = array(
+				'name' => 'none',
+				'title' => 'none',
+				'url' => "/timeWatch/setProject?project=none",
+			);
+			
+			$userMenu = array(
+				array(
+					'name' => 'chpass',
+					'title' => 'Change Password',
+					'url' => "/timeWatch/changePasswd",
+				),
+				array(
+					'name' => 'logout',
+					'title' => 'Log Off',
+					'url' => "/?logOut=logOut",
+				),
+			);
+			$timeWatchMenu = array(
+				array(
+					'name' => 'timeIn',
+					'title' => 'Time In',
+					'url' => "/timewatch/in",
+				),
+				array(
+					'name' => 'timeOut',
+					'title' => 'Time Out',
+					'url' => "/timewatch/out",
+				),
+				array(
+					'name' => 'timewatchThisMonth',
+					'title' => 'TimeWatch This Month',
+					'url' => "/timewatch",
+				),
+				array(
+					'name' => 'timeWatchSummary',
+					'title' => 'TimeWatch Summary',
+					'url' => "/timewatch/summary",
+				),
+			);
+
+		}
 		$adminMenu = array(
 			array(
 				'name' => 'showSource',
@@ -23,46 +92,12 @@ class Menu extends Mcontroller {
 				'target' => "clone",
 			),
 		);
-		$userMenu = array(
-			array(
-				'name' => 'chpass',
-				'title' => 'Change Password',
-				'url' => "/timeWatch/changePasswd",
-			),
-			array(
-				'name' => 'logout',
-				'title' => 'Log Off',
-				'url' => "/?logOut=logOut",
-			),
-		);
 
-		$timeWatchMenu = array(
-			array(
-				'name' => 'timeIn',
-				'title' => 'Time In',
-				'url' => "/timewatch/in",
-			),
-			array(
-				'name' => 'timeOut',
-				'title' => 'Time Out',
-				'url' => "/timewatch/out",
-			),
-			array(
-				'name' => 'timewatchThisMonth',
-				'title' => 'TimeWatch This Month',
-				'url' => "/timewatch",
-			),
-			array(
-				'name' => 'timeWatchSummary',
-				'title' => 'TimeWatch Summary',
-				'url' => "/timewatch/summary",
-			),
-		);
 
-		$loginName = TimeWatchLogin::loginName();
 		if ( $loginName )
 			$menu = array(
 				'timeWatch' => $timeWatchMenu,
+				$projectMenuTitle => $projectMenu,
 				'admin' => $adminMenu,
 				$loginName => $userMenu,
 			);
@@ -74,4 +109,3 @@ class Menu extends Mcontroller {
 	}
 	/*------------------------------------------------------------*/
 }
-
