931618a4d023d39bf1b11727f330918ca2a21bff
ohad <ohad@theora.com>
2020-12-19 10:35:32 +0200

    gitDiffs

--- src/Menu.class.php
+++ src/Menu.class.php
@@ -10,60 +10,65 @@ class Menu extends Mcontroller {
 	/*------------------------------------------------------------*/
 	/*------------------------------------------------------------*/
 	private function dd() {
-		$menu = array(
-			'admin' => array(
-				array(
-					'name' => 'showSource',
-					'title' => 'Show Source Code',
-					'url' => "/showSource",
-				),
-				array(
-					'name' => 'clone',
-					'title' => 'Clone',
-					'url' => "https://github.com/ohadaloni/timewatch",
-					'target' => "clone",
-				),
+		$adminMenu = array(
+			array(
+				'name' => 'showSource',
+				'title' => 'Show Source Code',
+				'url' => "/showSource",
+			),
+			array(
+				'name' => 'clone',
+				'title' => 'Clone',
+				'url' => "https://github.com/ohadaloni/timewatch",
+				'target' => "clone",
 			),
 		);
+		$userMenu = array(
+			array(
+				'name' => 'chpass',
+				'title' => 'Change Password',
+				'url' => "/timeWatch/changePasswd",
+			),
+			array(
+				'name' => 'logout',
+				'title' => 'Log Off',
+				'url' => "/?logOut=logOut",
+			),
+		);
+
+		$timeWatchMenu = array(
+			array(
+				'name' => 'timeIn',
+				'title' => 'Time In',
+				'url' => "/timewatch/in",
+			),
+			array(
+				'name' => 'timeOut',
+				'title' => 'Time Out',
+				'url' => "/timewatch/out",
+			),
+			array(
+				'name' => 'timewatchThisMonth',
+				'title' => 'TimeWatch This Month',
+				'url' => "/timewatch",
+			),
+			array(
+				'name' => 'timeWatchSummary',
+				'title' => 'TimeWatch Summary',
+				'url' => "/timewatch/summary",
+			),
+		);
+
 		$loginName = TimeWatchLogin::loginName();
 		if ( $loginName )
-			$menu[$loginName] = array(
-				array(
-					'name' => 'timeIn',
-					'title' => 'Time In',
-					'url' => "/timewatch/in",
-				),
-				array(
-					'name' => 'timeOut',
-					'title' => 'Time Out',
-					'url' => "/timewatch/out",
-				),
-				array(
-					'divider' => '-----------------------',
-				),
-				array(
-					'name' => 'timewatchThisMonth',
-					'title' => 'TimeWatch This Month',
-					'url' => "/timewatch",
-				),
-				array(
-					'name' => 'timeWatchSummary',
-					'title' => 'TimeWatch Summary',
-					'url' => "/timewatch/summary",
-				),
-				array(
-					'divider' => '-----------------------',
-				),
-				array(
-					'name' => 'chpass',
-					'title' => 'Change Password',
-					'url' => "/timeWatch/changePasswd",
-				),
-				array(
-					'name' => 'logout',
-					'title' => 'Log Off',
-					'url' => "/?logOut=logOut",
-				),
+			$menu = array(
+				'timeWatch' => $timeWatchMenu,
+				'admin' => $adminMenu,
+				$loginName => $userMenu,
+			);
+		else
+			$menu = array(
+				'admin' => $adminMenu,
 			);
 		return($menu);
 	}
