27afe201a925fc6b8e90ab72edca462a608570f8
ohad <ohad@theora.com>
2022-05-11 09:21:54 +0300

    gitDiffs

--- src/ShowSource.class.php
+++ src/ShowSource.class.php
@@ -8,7 +8,7 @@ class ShowSource extends TimeWatch {
 		if ( $file ) {
 			$source = highlight_file($file, true);
 		}
-		$this->Mview->showTpl("showSource/showSource.tpl",  array(
+		$this->Mview->showTpl("showSource/showSource.tpl", array(
 			'files' => $files,
 			'fileName' => $file,
 			'source' => @$source,
